93c56772473420d1483f55e49938891527675bf1
Dan Stoner <danstoner@gmail.com>
2015-10-10 15:52:36 -0400

    have a little window with some text

---
+++ playing_with_pygame/alert_and_button.py
@@ -0,0 +1,63 @@
+import pygame
+import time
+
+
+pygame.init()
+window = pygame.display.set_mode((300, 200))
+pygame.display.set_caption('Alerter with Snooze Button')
+
+
+# colors are specified using RGB or friendly names such as "white" or "grey"
+# see: http://cloford.com/resources/colours/namedcol.htm
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GRAY = (80, 80, 80)
+LIGHT_GRAY = (192, 192, 192)
+DARK_GRAY = (32, 32, 32)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+#backgroundcolor = (80,80,80)
+backgroundcolor = (DARK_GRAY)
+
+running = True
+
+def pollforquit():
+    event = pygame.event.poll()
+    if event.type == pygame.QUIT:
+        raise SystemExit
+    # put the process to sleep to share CPU and reduce resource consumption while idling
+    pygame.time.wait(15)  # time in ms
+
+
+# Initial screen decorations
+window.fill(backgroundcolor)
+# Fill background
+background = pygame.Surface(window.get_size())
+background = background.convert()
+background.fill(backgroundcolor)
+
+font = pygame.font.Font(None, 36)
+text = font.render("Stuff goes here", 1, BLUE)
+textpos = text.get_rect()
+textpos.centerx = background.get_rect().centerx
+background.blit(text, textpos)
+window.blit(background, (0, 0))
+
+pygame.display.flip()
+
+
+
+
+# Main Event Loop
+while running:
+    pollforquit();
+    
+
+
+
+raise SystemExit
+
+
