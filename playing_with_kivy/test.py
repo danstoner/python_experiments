4706b44d522e0ca817ae023a73ad8974de3412eb
Dan Stoner <danstoner@gmail.com>
2015-10-14 14:38:40 -0400

    layout and app window size

--- playing_with_kivy/test.py
+++ playing_with_kivy/test.py
@@ -1,6 +1,11 @@
 # test kivy app button and text input box
 
 from kivy.app import App
+from kivy.config import Config
+# set app size for smaller screens
+Config.set('graphics', 'width', 320)
+Config.set('graphics', 'height', 240)
+
 
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.textinput import TextInput
