e3529dec371fa764953509c989d7c6a759585f35
Dan Stoner <danstoner@gmail.com>
2015-10-19 21:34:52 -0400

    now can interrupt, cleaned up output, alarm now plays a sound

--- cat_alert.py
+++ cat_alert.py
@@ -1,6 +1,9 @@
 import os
 
-line = "*********************************"
+line = '''
+*****************************************
+'''
+
 acat = '''
                                  
       ,/|         _.--''^``-...___.._./;
@@ -20,8 +23,10 @@ def alert ():
     while alarming:
         print acat
         print line
-        # x gets the return code from the operating system command, success = 0
-        x = os.system("""bash -c 'read -s -t 1 -n 1 -p "Press ENTER to stop the alarm..."'""")
+        # bash command does two things:
+        #   first play an alert sound (/usr/share/sounds/purple/alert.wav from Ubuntu)
+        #   x receives the return code from the operating system "read" command, success = 0
+        x = os.system("""bash -c 'aplay -q alert.wav; read -s -t 1 -n 1 -p "Press ENTER to stop the alarm..."'""")
 
         print ""
         if x == 0: 
@@ -37,7 +42,16 @@ print line
 print "CAT DEFENSE SYSTEM INITIALIZED"
 print line
 
-while True:
-    input_code = raw_input ("Ready to scan: ")
-    if input_code == "abcdefg":
-        alert();
+try:
+    while True:
+        print "Press CTRL-C to abort (stop the program)"
+        print ""
+        input_code = raw_input ("Ready to scan: ")
+        if input_code == "abcdefg":
+            alert();
+except (KeyboardInterrupt, EOFError) as e:
+    print ""
+    print line
+    print "Stopping!"
+    print ""
+
