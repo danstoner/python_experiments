d801a61ff10bdeb515ae2f4b909059a71fa37d51
Dan Stoner <danstoner@gmail.com>
2015-10-26 15:10:21 -0400

    add env variable for pitft

--- PiTFT/pyg.py
+++ PiTFT/pyg.py
@@ -1,4 +1,7 @@
 import pyglet
+# SDL_FBDEV change is required for the Raspberry Pi PiTFT by AdaFruit
+import os
+os.environ["SDL_FBDEV"] = "/dev/fb1"
 
 window = pyglet.window.Window(300,200)
 image = pyglet.resource.image('umbrella.png')
